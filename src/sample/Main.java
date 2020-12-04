05c9087577d73a569e3235739529968104ae421f
gurk1sh <Gustaf_99@live.se>
2020-12-04 16:47:06 +0100

    landing page fix

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,14 +1,9 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        loadWindow("Inloggningssida.fxml", primaryStage);
+        loadWindow("Landingpage.fxml", primaryStage);
 
     }
 
